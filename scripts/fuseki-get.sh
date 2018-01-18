0eb3b263807501c460195ed54878b012d8c5a668
Adrian Gschwend <ktk@netlabs.org>
2018-01-18 21:03:25 +0100

    Remove Fuseki scripts, no longer necessary

--- scripts/fuseki-get.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -G -H "Accept: application/n-triples" -o target/incidents-inference.nt http://admin:cinderella@$FUSEKI_HOST:3030/bernmobil/data --data-urlencode graph=http://inference
--- scripts/fuseki-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-source scripts/env.sh
-curl -u admin:cinderella --data "dbType=tdb&dbName=bernmobil"  http://$FUSEKI_HOST:3030/$/datasets
-
-curl -X PUT -u admin:cinderella -T target/incidents-clean.nt -G -H "Content-Type: application/n-triples" http://admin:cinderella@$FUSEKI_HOST:3030/bernmobil/data
-
